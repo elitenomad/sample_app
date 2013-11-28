72646878f6080959018fb83e2612f68b95594227
elitenomad <stalin.pranava@gmail.com>
2013-11-28 12:17:24 +1100

    Finished edit,update and Delete of the users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
 		@user = User.find_by_email(params[:session][:email].downcase)
 		if @user && @user.authenticate(params[:session][:password])
 			sign_in @user
-			flash[:success] = "Welcome to the Sample App!"
-      		redirect_to @user
+			#flash[:success] = "Welcome to the Sample App!"
+      		#redirect_to @user
+      		redirect_back_or @user
 		else
 			flash.now[:error] = 'Invalid email/password combination' # Not quite right!
 			render 'new'
